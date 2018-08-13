a2d653617c66bbe4bf98db3b52536a29706faf30
EternYang <y312193268@gmail.com>
2018-08-13 11:54:50 +0800

    try to connect index,need use es-lint

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
-from django.conf.urls import include, url
-from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import re_path
+from django.conf.urls import include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
